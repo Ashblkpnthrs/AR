8e2b06212d6811662034e38cf3c1a77ae606aeaf
Ash <clement.coxam@coppernic.fr>
2019-11-08 17:04:27 +0100

    Initial commit

---
+++ app/src/main/java/fr/coppernic/arvideo/ArVideoFragment.kt
@@ -0,0 +1,217 @@
+package fr.coppernic.arvideo
+
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.ar.core.AugmentedImage
+import com.google.ar.core.AugmentedImageDatabase
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ExternalTexture
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import java.io.IOException
+
+open class ArVideoFragment : ArFragment() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var externalTexture: ExternalTexture
+    private lateinit var videoRenderable: ModelRenderable
+    private lateinit var videoAnchorNode: AnchorNode
+
+    private var activeAugmentedImage: AugmentedImage? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaPlayer = MediaPlayer()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+        arSceneView.isLightEstimationEnabled = false
+
+        initializeSession()
+        createArScene()
+
+        return view
+    }
+
+    override fun getSessionConfiguration(session: Session): Config {
+
+        fun loadAugmentedImageBitmap(imageName: String): Bitmap =
+            requireContext().assets.open(imageName).use { return BitmapFactory.decodeStream(it) }
+
+        fun setupAugmentedImageDatabase(config: Config, session: Session): Boolean {
+            try {
+                config.augmentedImageDatabase = AugmentedImageDatabase(session).also { db ->
+                    db.addImage(TEST_VIDEO_1, loadAugmentedImageBitmap(TEST_IMAGE_1))
+                }
+                return true
+            } catch (e: IllegalArgumentException) {
+                Log.e(TAG, "Could not add bitmap to augmented image database", e)
+            } catch (e: IOException) {
+                Log.e(TAG, "IO exception loading augmented image bitmap.", e)
+            }
+            return false
+        }
+
+        return super.getSessionConfiguration(session).also {
+            it.lightEstimationMode = Config.LightEstimationMode.DISABLED
+            it.focusMode = Config.FocusMode.AUTO
+
+            if (!setupAugmentedImageDatabase(it, session)) {
+                Toast.makeText(requireContext(), "Could not setup augmented image database", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun createArScene() {
+        // Create an ExternalTexture for displaying the contents of the video.
+        externalTexture = ExternalTexture().also {
+            mediaPlayer.setSurface(it.surface)
+        }
+
+        // Create a renderable with a material that has a parameter of type 'samplerExternal' so that
+        // it can display an ExternalTexture.
+        ModelRenderable.builder()
+            .setSource(requireContext(), R.raw.augmented_video_model)
+            .build()
+            .thenAccept { renderable ->
+                videoRenderable = renderable
+                renderable.isShadowCaster = false
+                renderable.isShadowReceiver = false
+                renderable.material.setExternalTexture("videoTexture", externalTexture)
+            }
+            .exceptionally { throwable ->
+                Log.e(TAG, "Could not create ModelRenderable", throwable)
+                return@exceptionally null
+            }
+
+        videoAnchorNode = AnchorNode().apply {
+            setParent(arSceneView.scene)
+        }
+    }
+
+    /**
+     * In this case, we want to support the playback of one video at a time.
+     * Therefore, if ARCore loses current active image FULL_TRACKING we will pause the video.
+     * If the same image gets FULL_TRACKING back, the video will resume.
+     * If a new image will become active, then the corresponding video will start from scratch.
+     */
+    override fun onUpdate(frameTime: FrameTime) {
+        val frame = arSceneView.arFrame ?: return
+
+        val updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage::class.java)
+
+        // If current active augmented image isn't tracked anymore and video playback is started - pause video playback
+        val nonFullTrackingImages = updatedAugmentedImages.filter { it.trackingMethod != AugmentedImage.TrackingMethod.FULL_TRACKING }
+        activeAugmentedImage?.let { activeAugmentedImage ->
+            if (isArVideoPlaying() && nonFullTrackingImages.any { it.index == activeAugmentedImage.index }) {
+                pauseArVideo()
+            }
+        }
+
+        val fullTrackingImages = updatedAugmentedImages.filter { it.trackingMethod == AugmentedImage.TrackingMethod.FULL_TRACKING }
+        if (fullTrackingImages.isEmpty()) return
+
+        // If current active augmented image is tracked but video playback is paused - resume video playback
+        activeAugmentedImage?.let { activeAugmentedImage ->
+            if (fullTrackingImages.any { it.index == activeAugmentedImage.index }) {
+                if (!isArVideoPlaying()) {
+                    resumeArVideo()
+                }
+                return
+            }
+        }
+
+        // Otherwise - make the first tracked image active and start video playback
+        fullTrackingImages.firstOrNull()?.let { augmentedImage ->
+            try {
+                playbackArVideo(augmentedImage)
+            } catch (e: Exception) {
+                Log.e(TAG, "Could not play video [${augmentedImage.name}]", e)
+            }
+        }
+    }
+
+    private fun isArVideoPlaying() = mediaPlayer.isPlaying
+
+    private fun pauseArVideo() {
+        videoAnchorNode.renderable = null
+        mediaPlayer.pause()
+    }
+
+    private fun resumeArVideo() {
+        mediaPlayer.start()
+        videoAnchorNode.renderable = videoRenderable
+    }
+
+    private fun dismissArVideo() {
+        videoAnchorNode.anchor?.detach()
+        videoAnchorNode.renderable = null
+        activeAugmentedImage = null
+        mediaPlayer.reset()
+    }
+
+    private fun playbackArVideo(augmentedImage: AugmentedImage) {
+        Log.d(TAG, "playbackVideo = ${augmentedImage.name}")
+
+        requireContext().assets.openFd(augmentedImage.name)
+            .use { descriptor ->
+                mediaPlayer.reset()
+                mediaPlayer.setDataSource(descriptor)
+            }.also {
+                mediaPlayer.isLooping = true
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+            }
+
+
+        videoAnchorNode.anchor?.detach()
+        videoAnchorNode.anchor = augmentedImage.createAnchor(augmentedImage.centerPose)
+        videoAnchorNode.localScale = Vector3(
+            augmentedImage.extentX, // width
+            1.0f,
+            augmentedImage.extentZ
+        ) // height
+
+        activeAugmentedImage = augmentedImage
+
+        externalTexture.surfaceTexture.setOnFrameAvailableListener {
+            it.setOnFrameAvailableListener(null)
+            videoAnchorNode.renderable = videoRenderable
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dismissArVideo()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    companion object {
+        private const val TAG = "ArVideoFragment"
+
+        private const val TEST_IMAGE_1 = "test_image_1.jpg"
+
+        private const val TEST_VIDEO_1 = "test_video_1.mp4"
+    }
+}
